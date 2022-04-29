7dc3d71468520b02a2af68af8cca9d44742bc7ab
Nathan Prater <nprater86@gmail.com>
2022-04-29 15:14:38 -0500

    changed some files

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask, flash, session
-import logging
 
 app = Flask(__name__)
 app.secret_key = 'zebra6328potato'
--- server.py
+++ server.py
@@ -1,5 +1,11 @@
 from flask_app.controllers import recipes, users
 from flask_app import app
+import logging
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
 if __name__=='__main__':
     app.run(debug=True)
